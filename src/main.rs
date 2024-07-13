f816cea6d25cebe995d984c12713410bca13faee
jdawg3636 <jdawg3636minecraft@gmail.com>
2024-07-12 22:48:01 -0500

    Added Solver, adjusted maze size

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,15 @@ mod maze;
 use maze::Maze;
 
 fn main() {
-    let width = 25;
-    let height = 25;
+    let width = 384;
+    let height = 128;
     let mut maze = Maze::new(width, height);
     maze.generate();
-    maze.save_as_png("maze.png");
+    maze.save_as_png("maze.png", None);
+
+    if let Some(solution) = maze.solve() {
+        maze.save_as_png("maze-solved.png", Some(&solution));
+    } else {
+        println!("No solution found for the maze.");
+    }
 }
--- src/maze.rs
+++ src/maze.rs
@@ -1,10 +1,30 @@
 use rand::prelude::SliceRandom;
 use image::{RgbImage, Rgb};
+use std::collections::BinaryHeap;
+use std::cmp::Ordering;
 
 pub struct Maze {
     width: usize,
     height: usize,
     cells: Vec<Vec<bool>>, // true for walls, false for paths
+}
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+struct Node {
+    position: (usize, usize),
+    cost: usize,
+}
+
+impl Ord for Node {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.cost.cmp(&self.cost) // reverse ordering for min-heap
+    }
+}
+
+impl PartialOrd for Node {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
 }
 
 impl Maze {
@@ -46,7 +66,52 @@ impl Maze {
         }
     }
 
-    pub fn save_as_png(&self, path: &str) {
+    fn heuristic(&self, (x1, y1): (usize, usize), (x2, y2): (usize, usize)) -> usize {
+        ((x1 as isize - x2 as isize).abs() + (y1 as isize - y2 as isize).abs()) as usize
+    }
+
+    pub fn solve(&self) -> Option<Vec<(usize, usize)>> {
+        let start = (0, 0);
+        let goal = (self.width - 1, self.height - 1);
+        let mut open_set = BinaryHeap::new();
+        open_set.push(Node { position: start, cost: 0 });
+
+        let mut came_from = vec![vec![None; self.height]; self.width];
+        let mut g_score = vec![vec![usize::MAX; self.height]; self.width];
+        g_score[start.0][start.1] = 0;
+
+        let directions = [(1, 0), (-1, 0), (0, 1), (0, -1)];
+
+        while let Some(Node { position, cost }) = open_set.pop() {
+            if position == goal {
+                let mut path = vec![position];
+                let mut current = position;
+                while let Some(prev) = came_from[current.0][current.1] {
+                    path.push(prev);
+                    current = prev;
+                }
+                path.reverse();
+                return Some(path);
+            }
+
+            for &(dx, dy) in &directions {
+                let neighbor = ((position.0 as isize + dx) as usize, (position.1 as isize + dy) as usize);
+                if neighbor.0 < self.width && neighbor.1 < self.height && !self.cells[neighbor.0][neighbor.1] {
+                    let tentative_g_score = g_score[position.0][position.1] + 1;
+                    if tentative_g_score < g_score[neighbor.0][neighbor.1] {
+                        came_from[neighbor.0][neighbor.1] = Some(position);
+                        g_score[neighbor.0][neighbor.1] = tentative_g_score;
+                        let f_score = tentative_g_score + self.heuristic(neighbor, goal);
+                        open_set.push(Node { position: neighbor, cost: f_score });
+                    }
+                }
+            }
+        }
+
+        None
+    }
+
+    pub fn save_as_png(&self, path: &str, solution: Option<&[(usize, usize)]>) {
         let mut img = RgbImage::new(self.width as u32, self.height as u32);
 
         for x in 0..self.width {
@@ -57,6 +122,12 @@ impl Maze {
                     Rgb([255, 255, 255]) // White for paths
                 };
                 img.put_pixel(x as u32, y as u32, color);
+            }
+        }
+
+        if let Some(solution) = solution {
+            for &(x, y) in solution {
+                img.put_pixel(x as u32, y as u32, Rgb([255, 0, 0])); // Red for solution path
             }
         }
 
